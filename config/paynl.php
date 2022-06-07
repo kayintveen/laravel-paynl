9e87bc6db5726b1471468d9fde8eb54f947b2b41
Deniz Tezcan <d.tezcan42@gmail.com>
2022-06-07 10:39:51 +0200

    Create paynl.php

---
+++ config/paynl.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+	'tokenCode' 	=> env('PAYNL_TOKENCODE', 'AT-####-####'),
+	'apiToken' 		=> env('PAYNL_APITOKEN', '****************************************'),
+	'serviceId' 	=> env('PAYNL_SERVICEID', 'SL-####-####'),
+];
